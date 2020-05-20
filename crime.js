4391096515b410161e683f6a41969f6d5280dfc8
ncfcdaniel <ncfcdaniel@outlook.com>
2020-05-20 16:20:35 +0100

    All files beautified

--- crime.js
+++ crime.js
@@ -13,56 +13,55 @@ app.appendChild(container);
 
 //Open a new connection, using the GET request on the URL endpoint 
 request.open('GET', 'https://data.police.uk/api/stops-street?lat=52.5695&lng=0.2405', true);
-request.onload = function() {
+request.onload = function () {
     //Begin accessing JSON data
     var data = JSON.parse(this.response);
-   if (request.status >=200 && request.status < 400)
-   {
-    data.forEach(movie => {
-        //Create a div with a card class
-        const card  = document.createElement('div');
-        card.setAttribute('class', 'card');
-        
-
-
-        //Create an H1 and set the text content to the film's title
-        const h1 = document.createElement('h5');
-        h1.textContent = movie.age_range;
-        
+    if (request.status >= 200 && request.status < 400) {
+        data.forEach(police => {
+            //Create a div with a card class
+            const card = document.createElement('div');
+            card.setAttribute('class', 'card');
 
 
 
-
-        const h2 = document.createElement('h6');
-        h2.textContent = `Release Date: ${movie.gender}` //End with an ellipses
+            //Create an H1 and set the text content to the film's title
+            const h1 = document.createElement('h5');
+            h1.textContent = `Age: ${police.age_range}`;
 
 
 
 
-        //Create a p and set the text content to the film's description
-        const p = document.createElement('p');
-        movie.legislation = movie.legislation.substring(0,300); //Limit to 300 chars so that the card doesn't become too long.
-        p.textContent = `${movie.legislation}...` //End with an ellipses
-        
-        //Append the cards to the container element
-        container.appendChild(card);
 
-        //Each card will contain an h1 heading a p paragraph
-        card.appendChild(h1);
-        card.appendChild(h2);
-        card.appendChild(p);
-
-        const errorMessage = document.createElement('marquee');
-        app.appendChild(errorMessage);
+            const h2 = document.createElement('h6');
+            h2.textContent = `Release Date: ${police.gender}` //End with an ellipses
 
 
-        //Log each movie's title
-        console.log(movie.title);
-        
-    });
-} else {
-    console.log('error');
-}
+
+
+            //Create a p and set the text content to the film's description
+            const p = document.createElement('p');
+            police.legislation = police.legislation.substring(0, 300); //Limit to 300 chars so that the card doesn't become too long.
+            p.textContent = `${police.legislation}...` //End with an ellipses
+
+            //Append the cards to the container element
+            container.appendChild(card);
+
+            //Each card will contain an h1 heading a p paragraph
+            card.appendChild(h1);
+            card.appendChild(h2);
+            card.appendChild(p);
+
+            const errorMessage = document.createElement('marquee');
+            app.appendChild(errorMessage);
+
+
+            //Log each police's title
+            console.log(police.title);
+
+        });
+    } else {
+        console.log('error');
+    }
 }
 
 
--- restaurants.js
+++ restaurants.js
@@ -13,24 +13,24 @@ app.appendChild(container);
 
 //Open a new connection, using the GET request on the URL endpoint 
 request.open('GET', 'https://developers.zomato.com/api/v2.1/search?apikey=e2d8e360811be83dfa09e9a026e11dc7&&entity_id=322&entity_type=city', true);
-request.onload = function() {
-    //Begin accessing JSON data
-    var data = JSON.parse(this.response);
-    var html = " ";
+request.onload = function () {
+  //Begin accessing JSON data
+  var data = JSON.parse(this.response);
+  var html = " ";
 
 
-    
-    data.quotesArray.forEach(function(val) {
-        var keys = Object.keys(val);
-          html += "<div class = 'text-output'>";
-            keys.forEach(function(key){ 
-                html += val[key];
 
-            });
-               html += "</div>"
-         });
+  data.quotesArray.forEach(function (val) {
+    var keys = Object.keys(val);
+    html += "<div class = 'text-output'>";
+    keys.forEach(function (key) {
+      html += val[key];
+
+    });
+    html += "</div>"
+  });
 
 
-       $(".realquote").html(html);
-   
-   }
+  $(".realquote").html(html);
+
+}
